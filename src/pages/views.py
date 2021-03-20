e16635ad11d9a6083b8c4f6b802bda4349f4e65f
Ashish Sapra <rajisingh@rajis-MacBook-Air.local>
2021-03-20 22:36:57 +0530

    Inital Commit

---
+++ src/pages/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home_view(request ,*args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	#return HttpResponse("<h1>Hello World</h1>")
+	return render(request, "home.html",{})
+def contact_view(request ,*args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	return render(request, "contact.html", {})
+
+def about_view(request ,*args, **kwargs):
+	#print(args, kwargs)
+	#print(request.user)
+	#return HttpResponse("<h1>Hello World</h1>")
+	return render(request, "about.html", {})
+
+def sc_view(request,*args,**kwargs):
+	return render(request, "sc.html", {})
