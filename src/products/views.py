037619df9af9aa3f07cefa619f78464b962fed37
Ashish Sapra <rajisingh@rajis-MacBook-Air.local>
2021-03-23 17:18:31 +0530

    Till 2:06

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'title': obj.title,
+        'desciption' : obj.desciption
+    }
+    request render(request, "product/detail.html", {})
