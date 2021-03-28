ff40ed0cf2cc03b231fae99ee55cfdeb2bd3098c
ash-13 <a.ash.bus@gmail.com>
2021-03-29 02:13:39 +0530

    Re-Commiting

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 # Create your views here.
 def product_detail_view(request):
@@ -30,7 +30,7 @@ def product_detail_view(request):
     return render(request, "products/product_create.html", context)
 '''
 
-def product_create_view(request):
+'''def product_create_view(request):
     #print(request.GET)
     #print(request.POST)
     if request.method == 'POST':
@@ -38,4 +38,14 @@ def product_create_view(request):
         print(my_new_title)
         # Product.objects.create(title=my_new_title)
     context = {}
+    return render(request, "products/product_create.html", context)
+'''
+
+def product_create_view(request):
+    my_form = RawProductForm()
+    context = {
+        "form" : my_form
+
+
+    }
     return render(request, "products/product_create.html", context)
